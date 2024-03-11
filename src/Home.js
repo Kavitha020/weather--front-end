bbfd31139d6ae65d48c8db38179f446809b8798e
Kavitha020 <kavisha020@gmail.com>
2024-03-11 05:36:53 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://wallpaper-house.com/data/out/12/wallpaper2you_542340.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
